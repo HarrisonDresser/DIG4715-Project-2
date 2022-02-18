2c52b85cde195101c5bf1684fa73b232f77d0648
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-02-17 21:14:54 -0500

    02/17 21:14

--- Assets/Script/PlayerScript/PlayerInteraction.cs
+++ Assets/Script/PlayerScript/PlayerInteraction.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    public bool isSwitch1Range;
-    public bool isSwitch2Range;
+    public bool isSwitch1InRange;
+    public bool isSwitch2InRange;
+    public bool isKeyInRange;
+    public bool hasKey;
 
     public GameObject movingDoor1;
     public GameObject movingDoor2;
     public GameObject movingDoor3;
+    public GameObject prisionDoor;
+    public GameObject key;
+
 
 
     void Start()
@@ -19,46 +24,64 @@ public class PlayerInteraction : MonoBehaviour
 
     void Update()
     {
-        MovingDoor();
-
+        DoorInteraction();
+        KeyInteraction(key);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "switch1")
-            isSwitch1Range = true;
+            isSwitch1InRange = true;
 
         if (other.gameObject.tag == "switch2")
-            isSwitch2Range = true;
+            isSwitch1InRange = true;
 
+        if (other.gameObject.tag == "key")
+            isKeyInRange = true;
     }
 
     void OnTriggerExit(Collider other)
     {
-        isSwitch1Range = false;
-        isSwitch2Range = false;
+        isSwitch2InRange = false;
+        isSwitch2InRange = false;
+        isKeyInRange = false;
     }
 
-    void MovingDoor()
+    void DoorInteraction()
     {
-        if (isSwitch1Range)
+        if (isSwitch1InRange)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 movingDoor1.SetActive(true);
                 movingDoor2.SetActive(false);
-                isSwitch1Range = false;
+                isSwitch1InRange = false;
             }
         }
 
 
-         if (isSwitch2Range)
+        if (isSwitch2InRange)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 movingDoor1.SetActive(true);
                 movingDoor3.SetActive(false);
                 movingDoor2.SetActive(true);
+            }
+        }
+    }
+
+    void KeyInteraction(GameObject key)
+    {
+        if (isKeyInRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                movingDoor1.SetActive(false);
+                movingDoor3.SetActive(false);
+                movingDoor2.SetActive(false);
+                Destroy(key);
+                hasKey = true;
             }
         }
     }
